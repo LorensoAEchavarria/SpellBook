4ae465d5c24da704069da2da11e35428108d4175
LorensoAEchavarria <43241718+LorensoAEchavarria@users.noreply.github.com>
2019-03-07 09:24:05 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+    $("#a").click(function() {
+$(".begone").slideToggle();
+});
+
+    $("#b").click(function() {
+$(".itsjustaprankbro").fadeToggle();
+});
+
+    $("#c").click(function() {
+$(".tough").toggle();
+});
